e08bb3b86deec47689dde9459f594cd81a71dd7e
whaleshade <whaleshade@gmail.com>
2023-04-20 21:01:14 +0900

    feat: axios로 정보 가져오기

--- src/components/pages/chat/channel-page/Channel.tsx
+++ src/components/pages/chat/channel-page/Channel.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 import Layout from '../../../commons/layout/Layout';
 
+const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: #3ab5c5;
+`;
+
 export default function Channel() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = process.env.REACT_APP_TOKEN;
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+
+      try {
+        const response = await axios.get(
+          'http://127.0.0.1:3000/channels',
+          config,
+        );
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Layout>
-      <div>Channel</div>
+      <List>
+        <h1>Channel</h1>
+        {data ? (
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        ) : (
+          <p>Loading data...</p>
+        )}
+        <h2>hello</h2>
+      </List>
     </Layout>
   );
 }
