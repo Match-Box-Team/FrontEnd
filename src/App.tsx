b4cb8ad931f6aa0ef2fb3fed0e8866a844bc26b4
Joo-hyun-Kang <autoba9687@gmail.com>
2023-04-20 17:19:02 +0900

    feat: 전체 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Layout from './components/commons/layout/Layout';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Layout>
+      <div>Hello, this is the main content!</div>
+    </Layout>
+
+    /* 레이아웃 헤더 푸터 사용 예시
+    <Layout Header={<Header />} Footer={<Footer />}>
+      <div>Hello, this is the main content!</div>
+    </Layout>
+    */
+  );
 }
 
 export default App;
--- src/components/commons/header/header.tsx
+++ src/components/commons/header/header.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export default function header() {
-  return (
-    <div>header</div>
-  )
+  return <div>header</div>;
 }
