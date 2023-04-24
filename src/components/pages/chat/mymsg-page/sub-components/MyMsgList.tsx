501cd855132305b3c964888efc994fda53c78620
whaleshade <whaleshade@gmail.com>
2023-04-24 21:08:55 +0900

    feat: 내가속한페이지 틀

---
+++ src/components/pages/chat/mymsg-page/sub-components/MyMsgList.tsx
@@ -0,0 +1,192 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { isErrorOnGet } from '../../../../../recoil/globals/atoms/atom';
+import ErrorPopup from '../../../../commons/error/ErrorPopup';
+import { getImageUrl } from '../../../../../api/ProfileImge';
+import { getChatRoomInfo } from '../../../../../api/ChatRoomInfo';
+
+const Outline = styled.ul`
+  display: flex;
+  margin-top: 0;
+  flex-direction: column;
+  align-items: center;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  gap: 0;
+  height: inherit;
+  overflow-y: auto;
+`;
+
+const List = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 10rem;
+  background-color: #ffffff;
+  border: 1px solid #d8d8d8;
+`;
+
+const RoomItem = styled.li`
+  width: 100%;
+  text-align: start;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const RoomTitle = styled.div`
+  font-size: 1.5rem;
+  color: #000000;
+  text-decoration: none;
+  color: inherit;
+  text-align: left;
+`;
+
+const RoomMemeber = styled.div`
+  font-size: 1.2rem;
+  color: #000000;
+  text-decoration: none;
+  color: inherit;
+  text-align: left;
+`;
+
+const ProfileImg = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background-color: #ad4545;
+  border-radius: 2.5rem;
+  overflow: hidden;
+`;
+
+const RoomBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const RoomInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+`;
+
+const LastMsgTime = styled.div`
+  font-size: 1.2rem;
+  color: #000000;
+  text-decoration: none;
+  color: inherit;
+`;
+
+const UnreadMsgCount = styled.div`
+  font-size: 1.2rem;
+  color: #000000;
+  text-decoration: none;
+  color: inherit;
+`;
+
+// interface IChannel {
+//   channelId: string;
+//   channelName: string;
+//   count: number;
+// }
+
+export default function MyMsgList() {
+  const [channels, setChannels] = useState<any[]>([]);
+  const [isErrorGet, setIsErrorGet] = useRecoilState(isErrorOnGet);
+  const [imageUrl, setImageUrl] = useState('');
+  const [myRooms, setMyRooms] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = process.env.REACT_APP_TOKEN;
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+
+      try {
+        const response = await axios.get(
+          'http://127.0.0.1:3000/channels/my',
+          config,
+        );
+        console.log(response.data);
+
+        const roomListPromises = response.data.channel.map(
+          async (channel: any) => {
+            const RoomList = await getChatRoomInfo(
+              channel.userChannel.channel.channelId,
+            );
+            return RoomList;
+          },
+        );
+
+        const tmpList: any[] = [];
+        const fetchedRoomLists = await Promise.all(roomListPromises);
+        const rooms = fetchedRoomLists.map(item => item.userChannel);
+        // console.log('rooms: ', rooms);
+        rooms.forEach(list => {
+          // console.log('prev: ', list[0].user.nickname);
+          tmpList.push(list);
+        });
+
+        setMyRooms(tmpList);
+        // console.log('tp ', tmpList);
+
+        // tmpList.forEach(list => {
+        //   // console.log('in ', list);
+        //   list.forEach((item: any) => {
+        //     // console.log(item.user.nickname);
+        //   });
+        // });
+
+        // console.log('channels: ', channels[0].userChannel);
+
+        const tmpUrl = await getImageUrl(
+          response.data.channel[0].user[0].user.userId,
+        );
+        setImageUrl(tmpUrl);
+        setChannels(response.data.channel);
+      } catch (error) {
+        setIsErrorGet(true);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {/* <ErrorPopup message="요청을 처리할 수 없습니다." /> */}
+      <Outline>
+        {channels.map((room, index) => (
+          <List key={room.userChannel.userChannelId}>
+            <RoomItem>
+              <ProfileImg>
+                <img src={imageUrl} alt="profile" width="100%" height="100%" />
+              </ProfileImg>
+              <RoomBody>
+                <RoomTitle>{room.userChannel.channel.channelName}</RoomTitle>
+                <RoomMemeber>
+                  {myRooms[index].map((item: any) => (
+                    <span key={item.user.userId}>{item.user.nickname}</span>
+                  ))}
+                </RoomMemeber>
+              </RoomBody>
+              <RoomInfo>
+                <LastMsgTime>14:42</LastMsgTime>
+                <UnreadMsgCount>1</UnreadMsgCount>
+              </RoomInfo>
+            </RoomItem>
+          </List>
+        ))}
+      </Outline>
+    </>
+  );
+}
