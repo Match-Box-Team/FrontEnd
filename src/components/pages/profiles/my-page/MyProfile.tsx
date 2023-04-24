23e8d67bde4e610f4a0cb2d84a40197600fd88e3
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-24 16:19:47 +0900

    feat: 프로필 수정 모달 틀 구성

--- src/components/pages/profiles/my-page/MyProfile.tsx
+++ src/components/pages/profiles/my-page/MyProfile.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../../commons/layout/Layout';
 import Footer from '../../../commons/footer/Footer';
 import Header from '../../../commons/header/Header';
+import EditMy from '../profile-modal/editmy-modal/EditMy';
 
 export default function MyProfile() {
-  return <div>my</div>;
-}
-
-/*
-    Header와 Footer를 넣었을 떄
-    return (
+  const [isOpenEditProfileModal, setIsOpenEditProfileModal] =
+    useState<boolean>(false);
+  const handleClickModal = () => {
+    setIsOpenEditProfileModal(!isOpenEditProfileModal);
+  };
+  return (
     <Layout Header={<Header title="MyPage" />} Footer={<Footer tab="my" />}>
-      <div>my</div>
+      <EditMy
+        isOpenEditProfileModal={isOpenEditProfileModal}
+        handleClickModal={handleClickModal}
+      />
     </Layout>
   );
-*/
+}
--- src/components/pages/profiles/profile-modal/editmy-modal/EditMy.tsx
+++ src/components/pages/profiles/profile-modal/editmy-modal/EditMy.tsx
@@ -1,1 +1,98 @@
-export const a = 1;
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import Popup, { XButton } from '../../../../commons/modals/popup-modal/Popup';
+import {
+  FormContainer,
+  FormDiv,
+  FormInput,
+  FormSubmitButton,
+  FormText,
+  FormWarp,
+} from '../../../chat/chat-modal/createroom-modal/Createroom';
+
+// 모달 prop 타입
+interface Props {
+  isOpenEditProfileModal: boolean;
+  handleClickModal: () => void;
+}
+
+export default function EditMy({
+  isOpenEditProfileModal,
+  handleClickModal,
+}: Props) {
+  // 선택된 이미지 초기화
+  const [selectedImage, setSelectedImage] = useState<string>('');
+
+  // nickname input 초기화
+  const [Nickname, setNickname] = useState<string>('');
+  // nickname input 업데이트
+  const handleNicknameChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    const { name, value } = event.target;
+    setNickname(value);
+  };
+
+  return (
+    <>
+      <XButton onClick={handleClickModal}>초대 모달 생성</XButton>;
+      {isOpenEditProfileModal && (
+        <Popup onClose={handleClickModal}>
+          <EditMyMainText>프로필 수정</EditMyMainText>
+          <FormDiv>
+            <FormWarp>
+              {/* <FormContainer onSubmit={handleSubmit}> */}
+              <FormContainer>
+                <SelectImageInput type="file" id="image-input" />
+                <FormText>닉네임 수정</FormText>
+                <FormInput
+                  type="text"
+                  name="channelName"
+                  value={Nickname}
+                  onChange={handleNicknameChange}
+                  placeholder="닉네임을 수정합니다"
+                  required
+                />
+                <FormSubmitButton type="submit">저장</FormSubmitButton>
+              </FormContainer>
+            </FormWarp>
+          </FormDiv>
+        </Popup>
+      )}
+    </>
+  );
+}
+
+const EditMyMainText = styled.p`
+  padding-top: 1rem;
+  color: #3f4d97;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+const SelectImageInput = styled.input`
+  font-family: 'NanumGothic';
+  align-self: center;
+  width: 8rem;
+  color: white;
+  background: #6d77af;
+  border-radius: 20px;
+  border: none;
+  margin-top: 10px;
+  padding: 10px;
+  cursor: pointer;
+`;
+
+const InputFile = styled.input`
+  font-family: 'NanumGothic';
+  align-self: center;
+  width: 8rem;
+  color: white;
+  background: #6d77af;
+  border-radius: 20px;
+  border: none;
+  margin-top: 10px;
+  padding: 10px;
+  cursor: pointer;
+`;
