4995954ff7118593a05e47f0b227b23488532e19
whaleshade <whaleshade@gmail.com>
2023-04-26 20:03:15 +0900

    feat: ready game 템플릿

--- src/components/pages/game/game-modal/readygame-modal/ReadyGame.tsx
+++ src/components/pages/game/game-modal/readygame-modal/ReadyGame.tsx
@@ -6,31 +6,35 @@ interface ReadyGameProps {
   onClick: () => void;
 }
 
-function ReadyGame({ onClick }: ReadyGameProps) {
+export default function ReadyGame({ onClick }: ReadyGameProps) {
   return (
-    <Layout>
-      <ModalWrapper>
-        <GameReady>
-          <h1>Play game!!</h1>
-        </GameReady>
-        <CloseButton type="button" onClick={onClick}>
-          &times;
-        </CloseButton>
-      </ModalWrapper>
-    </Layout>
+    <ModalWrapper>
+      <CloseButton type="button" onClick={onClick}>
+        &times;
+      </CloseButton>
+      <GameReady>GAME READY</GameReady>
+      <GameInfo />
+      <GamePlayers />
+      <GameMaps>
+        <SignPost />
+        <GameMapFlow />
+      </GameMaps>
+      <GameStart />
+    </ModalWrapper>
   );
 }
 
 const ModalWrapper = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
+  position: relative;
+  top: 50%;
+  transform: translateY(-53%);
   width: 100%;
   height: 100%;
   background-color: #e1e3ee;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const GameReady = styled.div`
@@ -39,20 +43,62 @@ const GameReady = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  background-color: #6d77af;
-  padding: 20px;
-  border-radius: 10px;
-  position: relative;
+  height: 10%;
+  font-size: 5rem;
+  font-weight: 900;
+  margin-top: 5rem;
+  margin-bottom: 1.5rem;
+  color: #3f4d97;
 `;
 
 const CloseButton = styled.button`
   position: absolute;
-  top: 10px;
-  right: 10px;
-  font-size: 20px;
+  top: 1.5rem;
+  right: 1.5rem;
+  font-size: 3rem;
+  font-weight: 900;
   background: none;
+  color: grey;
   border: none;
   cursor: pointer;
 `;
 
-export default ReadyGame;
+const GameInfo = styled.div`
+  width: 90%;
+  height: 20%;
+  background-color: #313c7a;
+  border-radius: 3rem;
+`;
+
+const GamePlayers = styled.div`
+  width: 100%;
+  height: 20%;
+  background-color: #e1e3ee;
+`;
+
+const GameMaps = styled.div`
+  width: 100%;
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SignPost = styled.div`
+  width: 40%;
+  height: 25%;
+  background-color: #6d77af;
+  margin-left: 2rem;
+  border-radius: 2rem 2rem 0 0;
+`;
+
+const GameMapFlow = styled.div`
+  width: 100%;
+  height: 75%;
+  background-color: #6d77af;
+`;
+
+const GameStart = styled.div`
+  width: 100%;
+  height: 20%;
+  background-color: #e1e3ee;
+`;
