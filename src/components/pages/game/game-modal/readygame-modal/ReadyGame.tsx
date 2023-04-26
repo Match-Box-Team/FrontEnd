fe4f67b12114dbf381a39753d26ccb477bc1c8a3
whaleshade <whaleshade@gmail.com>
2023-04-26 21:22:08 +0900

    feat:start 버튼

--- src/components/pages/game/game-modal/readygame-modal/ReadyGame.tsx
+++ src/components/pages/game/game-modal/readygame-modal/ReadyGame.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
-import Layout from '../../../../commons/layout/Layout';
+import styled, { keyframes } from 'styled-components';
+
+const clickAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(0.95);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
 
 interface ReadyGameProps {
   onClick: () => void;
@@ -13,13 +23,22 @@ export default function ReadyGame({ onClick }: ReadyGameProps) {
         &times;
       </CloseButton>
       <GameReady>GAME READY</GameReady>
-      <GameInfo />
-      <GamePlayers />
+      <GameInfo>
+        <GameIcon />
+        <GameName />
+      </GameInfo>
+      <GamePlayers>
+        <Player1 />
+        <Vs>VS</Vs>
+        <Player2 />
+      </GamePlayers>
       <GameMaps>
-        <SignPost />
-        <GameMapFlow />
+        <SignPost>MAP</SignPost>
+        <GameMapFlow>{/* <MapList /> */}</GameMapFlow>
       </GameMaps>
-      <GameStart />
+      <GameStart>
+        <StartButton>START</StartButton>
+      </GameStart>
     </ModalWrapper>
   );
 }
@@ -68,12 +87,64 @@ const GameInfo = styled.div`
   height: 20%;
   background-color: #313c7a;
   border-radius: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const GameIcon = styled.div`
+  width: 30%;
+  height: 80%;
+  background-color: #313c7a;
+  margin-top: 1rem;
+  margin-left: 2rem;
+`;
+
+const GameName = styled.div`
+  width: 50%;
+  height: 80%;
+  background-color: #313c7a;
+  margin-top: 1rem;
+  margin-right: 2rem;
 `;
 
 const GamePlayers = styled.div`
   width: 100%;
   height: 20%;
   background-color: #e1e3ee;
+  display: flex;
+  align-items: space-between;
+  padding-left: 2rem;
+  padding-right: 2rem;
+`;
+
+const Player1 = styled.div`
+  width: 40%;
+  height: 80%;
+  background-color: #e1e3ee;
+  margin-top: 1rem;
+`;
+
+const Player2 = styled.div`
+  width: 40%;
+  height: 80%;
+  background-color: #e1e3ee;
+  margin-top: 1rem;
+`;
+
+const Vs = styled.div`
+  width: 20%;
+  height: 80%;
+  background-color: #e1e3ee;
+  margin-top: 1rem;
+  color: red;
+  -webkit-text-stroke-width: 0.2rem;
+  -webkit-text-stroke-color: black;
+  font-size: 4rem;
+  font-weight: 900;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const GameMaps = styled.div`
@@ -86,9 +157,16 @@ const GameMaps = styled.div`
 const SignPost = styled.div`
   width: 40%;
   height: 25%;
-  background-color: #6d77af;
   margin-left: 2rem;
+  background-color: #6d77af;
+  color: white;
+  font-size: 4rem;
+  font-weight: 900;
+  padding-top: 1rem;
   border-radius: 2rem 2rem 0 0;
+  z-index: 10;
+  position: relative;
+  top: 0.25px;
 `;
 
 const GameMapFlow = styled.div`
@@ -101,4 +179,20 @@ const GameStart = styled.div`
   width: 100%;
   height: 20%;
   background-color: #e1e3ee;
+`;
+
+const StartButton = styled.button`
+  width: 70%;
+  height: 70%;
+  background-color: red;
+  margin-top: 2rem;
+  border-radius: 4rem;
+  border-color: white;
+  border-width: 1rem;
+  color: white;
+  font-size: 5rem;
+  font-weight: 900;
+  &:active {
+    animation: ${clickAnimation} 0.2s;
+  }
 `;
