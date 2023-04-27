8bd83fa65b716e598a576c4ce6f9dd7b18a50f43
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-27 16:11:56 +0900

    feat: 게임 구매 모달 초안

--- src/components/pages/game/game-modal/buygame-modal/BuyGame.tsx
+++ src/components/pages/game/game-modal/buygame-modal/BuyGame.tsx
@@ -1,5 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
+import { NoXPopup } from '../../../../commons/modals/popup-modal/Popup';
 
-export default function Buygame() {
-  return <div>buygame</div>;
+interface Props {
+  isOpenBuyGameModal: boolean;
+  handleClickModal: () => void;
 }
+
+interface ButtonProps {
+  yes: boolean;
+}
+
+export default function BuyGame({
+  isOpenBuyGameModal,
+  handleClickModal,
+}: Props) {
+  return (
+    <div>
+      {isOpenBuyGameModal && (
+        <NoXPopup onClose={handleClickModal}>
+          <MainText>정말로 구매하시겠습니까</MainText>
+          <BuyButton yes>네</BuyButton>
+          <BuyButton yes={false}>아니오</BuyButton>
+        </NoXPopup>
+      )}
+    </div>
+  );
+}
+
+const MainText = styled.p`
+  padding-top: 1rem;
+  font-size: 2rem;
+`;
+
+export const BuyButton = styled.button<ButtonProps>`
+  font-family: 'NanumGothic';
+  font-weight: bold;
+  margin-top: 1rem;
+  align-self: center;
+  width: 7rem;
+  color: white;
+  ${({ yes }) => {
+    if (yes) {
+      return `background: #da0d00`;
+    }
+    return `background: #313c7a`;
+  }}
+  border-radius: 10px;
+  border: none;
+  margin-top: 15px;
+  margin-bottom: 10px;
+  padding: 7px;
+  cursor: pointer;
+`;
