a7a3f63a0a16690c9b4af3a780b7b17af82a817f
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-27 17:32:05 +0900

    feat: 게임 구매 모달

--- src/components/pages/game/game-modal/buygame-modal/BuyGame.tsx
+++ src/components/pages/game/game-modal/buygame-modal/BuyGame.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
 import { NoXPopup } from '../../../../commons/modals/popup-modal/Popup';
+import { userState } from '../../../../../recoil/locals/login/atoms/atom';
 
 interface Props {
   isOpenBuyGameModal: boolean;
   handleClickModal: () => void;
+  gameId: string;
 }
 
 interface ButtonProps {
@@ -14,14 +18,39 @@ interface ButtonProps {
 export default function BuyGame({
   isOpenBuyGameModal,
   handleClickModal,
+  gameId,
 }: Props) {
+  // 유저  정보
+  const userInfo = useRecoilValue(userState);
+
+  // 게임 구매
+  const handleBuyGame = () => {
+    const buyGame = async () => {
+      const data = await axios.post(
+        `http://localhost:3000/games/${gameId}/buy`,
+        null,
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        },
+      );
+    };
+    buyGame();
+    handleClickModal();
+    window.location.reload();
+  };
   return (
     <div>
       {isOpenBuyGameModal && (
         <NoXPopup onClose={handleClickModal}>
           <MainText>정말로 구매하시겠습니까</MainText>
-          <BuyButton yes>네</BuyButton>
-          <BuyButton yes={false}>아니오</BuyButton>
+          <ButtonsWrap>
+            <BuyButton yes onClick={() => handleBuyGame()}>
+              네
+            </BuyButton>
+            <BuyButton yes={false} onClick={() => handleClickModal()}>
+              아니오
+            </BuyButton>
+          </ButtonsWrap>
         </NoXPopup>
       )}
     </div>
@@ -29,27 +58,32 @@ export default function BuyGame({
 }
 
 const MainText = styled.p`
-  padding-top: 1rem;
-  font-size: 2rem;
+  margin: 30px 0px;
+  color: black;
+  font-size: 1.8rem;
+  font-weight: bolder;
+  width: 20rem;
 `;
 
-export const BuyButton = styled.button<ButtonProps>`
+const ButtonsWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 18rem;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const BuyButton = styled.button<ButtonProps>`
   font-family: 'NanumGothic';
+  font-size: 1.4rem;
   font-weight: bold;
-  margin-top: 1rem;
   align-self: center;
   width: 7rem;
   color: white;
-  ${({ yes }) => {
-    if (yes) {
-      return `background: #da0d00`;
-    }
-    return `background: #313c7a`;
-  }}
+  background-color: ${({ yes }) => (yes ? '#da0d00' : '#313c7a')};
   border-radius: 10px;
   border: none;
-  margin-top: 15px;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
   padding: 7px;
   cursor: pointer;
 `;
