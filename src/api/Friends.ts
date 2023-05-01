f2a5193c12882c15cf1d8ac0dfd94d30222237b2
Joo-hyun-Kang <autoba9687@gmail.com>
2023-05-01 17:39:33 +0900

    feat: 친구 리스트 채널 구현

--- src/api/Friends.ts
+++ src/api/Friends.ts
@@ -2,23 +2,28 @@ import { useQuery } from 'react-query';
 import { AxiosError, AxiosResponse } from 'axios';
 import { useAxiosWithToken } from '.';
 
-// 예시
-interface IFriend {
+interface IBuddy {
+  nickname: string;
+  intraId: string;
+  status: string;
+  image: string;
+}
+
+interface IFriends {
   friendId: string;
   buddyId: string;
-  buddy: {
-    nickname: string;
-    intraId: string;
-    status: string;
-    image: string;
-  };
+  buddy: IBuddy;
+}
+
+interface IFriend {
+  friends: IFriends[];
 }
 
 export const useGetFriendList = () => {
   const axiosInstance = useAxiosWithToken();
 
-  return useQuery<IFriend[], AxiosError>('friends', async () => {
-    const response: AxiosResponse<IFriend[]> = await axiosInstance.get(
+  return useQuery<IFriend, AxiosError>('friends', async () => {
+    const response: AxiosResponse<IFriend> = await axiosInstance.get(
       '/friends',
     );
     return response.data;
--- src/components/pages/friend/friendlist-page/FriendList.tsx
+++ src/components/pages/friend/friendlist-page/FriendList.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import Layout from '../../../commons/layout/Layout';
 import Footer from '../../../commons/footer/Footer';
 import Header from '../../../commons/header/Header';
+import MyProfile from './components/MyProfile';
+import AddFriends from './components/AddFriends';
+import FriendDetail from './components/FrinedDetail';
+
+const Base = styled.div`
+  position: relative;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.8rem;
+  align-items: start;
+  padding: 0 2.4rem;
+`;
 
 export default function FriendList() {
-  return <div>friendlist</div>;
-}
-
-/*
-    Header와 Footer를 넣었을 떄
-    return (
+  return (
     <Layout
       Header={<Header title="Friends" friendToggle toggleMove />}
       Footer={<Footer tab="friend" />}
     >
-      <div>friendlist</div>
+      <Base>
+        <Container>
+          <MyProfile />
+          <AddFriends />
+        </Container>
+        <FriendDetail />
+      </Base>
     </Layout>
   );
-*/
+}
